1546f09f636ea3a42666d67c3a004428a246ce10
Naman327 <nj590074@gmail.com>
2023-01-09 12:05:53 +0530

    additiopon program

---
+++ addition.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std ; 
+
+int main(){
+    int a,b ;
+    cin>>a;
+    cin>>b ; 
+
+    int x ; 
+    x =a+b ; 
+    cout<<"Addition of "<<a<<"and"<<b<<"is"<<x<<endl ; 
+
+    return 0 ; 
+     
+
+}
